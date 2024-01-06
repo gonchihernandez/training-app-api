ff5fd959dad433909d9a5de4a7ecbfaab1e6685e
Gonzalo Hernandez <gonchihernandez@Gonzalos-MacBook-Pro.local>
2024-01-06 18:25:54 -0300

    Add JWT const

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 export const PORT = process.env.PORT || 3000;
 
+export const JWT = process.env.JWT || 'xyz123';
 export const PG_PORT = process.env.PG_PORT || 5432;
-export const PG_HOST = process.env.PG_HOST || "localhost";
-export const PG_USER = process.env.PG_USER || "postgres";
-export const PG_PASSWORD = process.env.PG_PASSWORD || "mysecretpassword";
-export const PG_DATABASE = process.env.PG_DATABASE || "tasksdb";
+export const PG_HOST = process.env.PG_HOST || 'localhost';
+export const PG_USER = process.env.PG_USER || 'postgres';
+export const PG_PASSWORD = process.env.PG_PASSWORD || 'admin';
+export const PG_DATABASE = process.env.PG_DATABASE || 'TrainingApp';
 
-export const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
+export const ORIGIN = process.env.ORIGIN || 'http://localhost:5173';
